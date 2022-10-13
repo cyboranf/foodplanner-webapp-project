d30a6e04f876404de0f2b3901f03de57509fbe29
cyboranf <cyboranf@gmail.com>
2022-10-13 15:30:58 +0200

    Showing recipes back and View -done

--- src/main/java/com/example/foodplannerproject/service/RecipeService.java
+++ src/main/java/com/example/foodplannerproject/service/RecipeService.java
@@ -1,4 +1,27 @@
 package com.example.foodplannerproject.service;
 
+import com.example.foodplannerproject.domain.Recipe;
+import com.example.foodplannerproject.repository.RecipeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class RecipeService {
+    private final RecipeRepository recipeRepository;
+
+    public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public Recipe saveRecipe(Recipe recipe){
+        recipeRepository.save(recipe);
+        return recipe;
+    }
+
+    public List<Recipe> findAll(){
+        return recipeRepository.findAll();
+    }
 }
