eaf0e9ce4a3a67d2a221131da63452ce29bf9613
cyboranf <cyboranf@gmail.com>
2022-10-17 16:45:30 +0200

    PlanService -updated

--- src/main/java/com/example/foodplannerproject/service/PlanService.java
+++ src/main/java/com/example/foodplannerproject/service/PlanService.java
@@ -1,4 +1,22 @@
 package com.example.foodplannerproject.service;
 
+import com.example.foodplannerproject.domain.Plan;
+import com.example.foodplannerproject.repository.PlanRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class PlanService {
+    private final PlanRepository planRepository;
+
+    public PlanService(PlanRepository planRepository) {
+        this.planRepository = planRepository;
+    }
+
+    public List<Plan> findAll(){
+        return planRepository.findAll();
+    }
 }
