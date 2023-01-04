b9a1d3e33f0384277620ebec940088ba4df219cc
cyboranf <cyboranf@gmail.com>
2023-01-04 09:50:22 +0100

    LogoutController -done

--- src/main/java/com/example/foodplannerproject/web/LogoutController.java
+++ src/main/java/com/example/foodplannerproject/web/LogoutController.java
@@ -6,17 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class LogoutController {
     @GetMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response){
-        Cookie[] cookies=request.getCookies();
-        for (Cookie cookie:cookies){
-            cookie.setMaxAge(0);
-            response.addCookie(cookie);
-        }
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        List<Cookie> cookieList = Arrays.asList(cookies);
 
+        cookieList.forEach(c -> {
+            c.setMaxAge(0);
+            response.addCookie(c);
+        });
 
         return "redirect:/";
     }
