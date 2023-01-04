4bbd9869ead94d754ed0ae0fa1355b0a4b48eaab
cyboranf <cyboranf@gmail.com>
2023-01-04 11:00:28 +0100

    Method to return quantity of recipes -added

--- src/main/java/com/example/foodplannerproject/repository/RecipeRepository.java
+++ src/main/java/com/example/foodplannerproject/repository/RecipeRepository.java
@@ -3,6 +3,7 @@ package com.example.foodplannerproject.repository;
 
 import com.example.foodplannerproject.domain.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,8 +12,10 @@ import java.util.Optional;
 public interface RecipeRepository extends JpaRepository<Recipe,Long> {
     Recipe save(Recipe recipe);
 
-
     void deleteById(Long Id);
 
     Optional<Recipe> findAllById(Long id);
+
+    @Query("SELECT COUNT(r) FROM Recipe r")
+    Integer quantityOfAllRecipes();
 }
