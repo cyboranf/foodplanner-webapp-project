93da5b7442dc9bc6ae483aa622d89e2f53b97dfd
cyboranf <cyboranf@gmail.com>
2023-01-04 09:03:16 +0100

    Added View to /recipes endpoint -done

--- src/main/java/com/example/foodplannerproject/web/RecipesController.java
+++ src/main/java/com/example/foodplannerproject/web/RecipesController.java
@@ -1,29 +1,13 @@
 package com.example.foodplannerproject.web;
 
-import com.example.foodplannerproject.domain.Recipe;
-import com.example.foodplannerproject.service.RecipeService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 @Controller
 public class RecipesController {
-    private final RecipeService recipeService;
-
-    public RecipesController(RecipeService recipeService) {
-        this.recipeService = recipeService;
-    }
-
     @GetMapping("/recipes")
-    public ModelAndView show(HttpServletResponse response,
-                             HttpServletRequest request){
-        List<Recipe> recipeList=recipeService.findAll();
-        request.setAttribute("recipeList",recipeList);
-
-        return new ModelAndView("recipes");
+    public String showAllRecipes() {
+        return "recipes";
     }
 }
