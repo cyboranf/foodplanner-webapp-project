d40db932e7d0cea41b9ef4f813d17c442ae4a286
cyboranf <cyboranf@gmail.com>
2023-01-04 11:21:54 +0100

    ModelAttribute ListOfRecipes -added

--- src/main/java/com/example/foodplannerproject/web/RecipesController.java
+++ src/main/java/com/example/foodplannerproject/web/RecipesController.java
@@ -1,13 +1,34 @@
 package com.example.foodplannerproject.web;
 
 
+import com.example.foodplannerproject.domain.Recipe;
+import com.example.foodplannerproject.service.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class RecipesController {
+    private final RecipeService recipeService;
+
+    public RecipesController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
     @GetMapping("/recipes")
-    public String showAllRecipes() {
+    public String showAllRecipes(Model model) {
+        List<Recipe> recipes = recipeService.findAll();
+        model.addAttribute("recipeList", recipes);
+
         return "recipes";
     }
+    @GetMapping("/app/recipe/list")
+    public String showAllRecipesApp(Model model){
+        List<Recipe> recipes = recipeService.findAll();
+        model.addAttribute("recipeList", recipes);
+
+        return "recipes";
+    }
+
 }
