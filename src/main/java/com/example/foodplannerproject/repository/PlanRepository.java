bdad716dc345e774e535842dadd413ec1d103e29
cyboranf <cyboranf@gmail.com>
2023-01-04 11:00:49 +0100

    Method to return quantity of plans -added

--- src/main/java/com/example/foodplannerproject/repository/PlanRepository.java
+++ src/main/java/com/example/foodplannerproject/repository/PlanRepository.java
@@ -3,6 +3,7 @@ package com.example.foodplannerproject.repository;
 
 import com.example.foodplannerproject.domain.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +16,7 @@ public interface PlanRepository extends JpaRepository<Plan, Long> {
     Optional<Plan> findAllById(Long id);
 
     void deleteById(Long id);
+
+    @Query("SELECT COUNT(p) FROM Plan p")
+    Integer quantityOfAllPlans();
 }
