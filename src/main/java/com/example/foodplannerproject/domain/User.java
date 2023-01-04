ce2222d2aaecea23d8ec23ed1df9ab1806932e6a
cyboranf <cyboranf@gmail.com>
2023-01-04 09:25:58 +0100

    User entity -changed

--- src/main/java/com/example/foodplannerproject/domain/User.java
+++ src/main/java/com/example/foodplannerproject/domain/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -23,7 +24,7 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email")
+    @Column(name = "email", unique = true)
     private String email;
 
     @Column(name = "password")
