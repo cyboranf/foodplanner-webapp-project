75f985dc67758bfe1450e5f3e26da9c413f4dae3
cyboranf <cyboranf@gmail.com>
2023-01-04 09:04:09 +0100

    Added View to /registration endpoint -done

--- src/main/java/com/example/foodplannerproject/web/RegisterController.java
+++ src/main/java/com/example/foodplannerproject/web/RegisterController.java
@@ -1,23 +1,14 @@
 package com.example.foodplannerproject.web;
 
-
-import com.example.foodplannerproject.domain.Admin;
-import com.example.foodplannerproject.domain.User;
-import com.example.foodplannerproject.service.AdminService;
-import com.example.foodplannerproject.service.UserService;
-import com.sun.istack.NotNull;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class RegisterController {
+    @GetMapping("/registration")
+    public String showViewOfRegister() {
+        return "register";
+    }
 
 }
