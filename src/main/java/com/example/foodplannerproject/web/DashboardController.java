b4ae50b6bc3aef14f6135b8cf072f17fce1a43f5
cyboranf <cyboranf@gmail.com>
2023-01-04 11:02:40 +0100

    /app/dashboard view with attributes -added

--- src/main/java/com/example/foodplannerproject/web/DashboardController.java
+++ src/main/java/com/example/foodplannerproject/web/DashboardController.java
@@ -1,12 +1,34 @@
 package com.example.foodplannerproject.web;
 
+import com.example.foodplannerproject.domain.Plan;
+
+import com.example.foodplannerproject.service.PlanService;
+import com.example.foodplannerproject.service.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class DashboardController {
+    private final PlanService planService;
+    private final RecipeService recipeService;
+
+    public DashboardController(PlanService planService, RecipeService recipeService) {
+        this.planService = planService;
+        this.recipeService = recipeService;
+    }
+
     @GetMapping("/app/dashboard")
-    public String viewOfDashboardJsp() {
+    public String viewOfDashboardJsp(Model model) {
+        List<Plan> plans = planService.findAll();
+        Plan plan = plans.get(plans.size() - 1);
+
+        model.addAttribute("numberOfPlans", planService.quantityOfPlans());
+        model.addAttribute("numberOfRecipe", recipeService.countOfRecipes());
+        model.addAttribute("lastPlan", plan);
+
         return "dashboard";
     }
 }
