9f88868b31cb22a7ae133eaacf50aa32c3b16942
cyboranf <cyboranf@gmail.com>
2023-01-05 12:13:30 +0100

    method to add plans -added

--- src/main/java/com/example/foodplannerproject/web/PlansController.java
+++ src/main/java/com/example/foodplannerproject/web/PlansController.java
@@ -5,7 +5,11 @@ import com.example.foodplannerproject.service.PlanService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -22,5 +26,26 @@ public class PlansController {
         model.addAttribute("planList", planList);
 
         return "appPlanList";
+    }
+
+    @GetMapping("/app/plan/add")
+    public String showPlanAdd() {
+        return "appPlanAdd";
+    }
+
+    @PostMapping("/app/plan/add")
+    public String savingNewPlan(@RequestParam String planName,
+                                @RequestParam String planDescription) {
+        Plan newPlan = new Plan();
+        LocalDate created = LocalDate.now();
+        if (planName.isEmpty() || planDescription.isEmpty()) {
+            return "appPlanAdd";
+        } else {
+            newPlan.setName(planName);
+            newPlan.setDescription(planDescription);
+            newPlan.setCreated(String.valueOf(created));
+            planService.save(newPlan);
+            return "redirect:/app/dashboard";
+        }
     }
 }
