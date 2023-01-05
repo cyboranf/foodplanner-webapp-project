33302bf3d104f99d4f466c0056dea9e4c8a9a5d1
cyboranf <cyboranf@gmail.com>
2023-01-05 12:11:24 +0100

    Method to add recipe -added

--- src/main/java/com/example/foodplannerproject/web/RecipesController.java
+++ src/main/java/com/example/foodplannerproject/web/RecipesController.java
@@ -6,6 +6,8 @@ import com.example.foodplannerproject.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,6 +18,7 @@ public class RecipesController {
     public RecipesController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
+
     @GetMapping("/recipes")
     public String showAllRecipes(Model model) {
         List<Recipe> recipes = recipeService.findAll();
@@ -23,12 +26,38 @@ public class RecipesController {
 
         return "recipes";
     }
+
     @GetMapping("/app/recipe/list")
-    public String showAllRecipesApp(Model model){
+    public String showAllRecipesApp(Model model) {
         List<Recipe> recipes = recipeService.findAll();
         model.addAttribute("recipeList", recipes);
 
         return "recipes";
+    }
+
+    @GetMapping("/app/recipe/add")
+    public String showRecipeAdd() {
+        return "appRecipeAdd";
+    }
+
+    @PostMapping("/app/recipe/add")
+    public String saveNewRecipe(@RequestParam String name,
+                                @RequestParam String description,
+                                @RequestParam String preparationTime,
+                                @RequestParam String preparation,
+                                @RequestParam String ingredients) {
+        if (name.isEmpty() || description.isEmpty() || preparationTime.isEmpty() || preparation.isEmpty() || ingredients.isEmpty()) {
+            return "appRecipeAdd";
+        } else {
+            Recipe newRecipe = new Recipe();
+            newRecipe.setName(name);
+            newRecipe.setDescription(description);
+            newRecipe.setPreparationTime(Integer.parseInt(preparationTime));
+            newRecipe.setIngredients(ingredients);
+
+            recipeService.saveRecipe(newRecipe);
+            return "redirect:/app/dashboard";
+        }
     }
 
 }
